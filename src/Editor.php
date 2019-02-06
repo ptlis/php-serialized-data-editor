23e7935ee14ecb1b3d7398c5b84d15bf2acec93d
ptlis <ptlis@ptlis.net>
2019-02-06 16:47:11 +0000

    Add basic search & replace editor methods

---
+++ src/Editor.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright   (c) 2017-present brian ridley
+ * @author      brian ridley <ptlis@ptlis.net>
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\SerializedDataEditor;
+
+use ptlis\SerializedDataEditor\Tokenizer\Token;
+use ptlis\SerializedDataEditor\Tokenizer\Tokenizer;
+
+/**
+ * Allows editing of serialized datastructures.
+ */
+final class Editor
+{
+    public function contains(
+        string $serializedData,
+        string $searchTerm
+    ): bool {
+        $tokenizer = new Tokenizer();
+
+        $containsString = false;
+        foreach ($tokenizer->tokenize($serializedData) as $token) {
+            if (Token::STRING === $token->getType() && strstr($token->getValue(), $searchTerm)) {
+                $containsString = true;
+            }
+        }
+
+        return $containsString;
+    }
+
+    public function containsCount(
+        string $serializedData,
+        string $searchTerm
+    ): int {
+        $tokenizer = new Tokenizer();
+
+        $count = 0;
+        foreach ($tokenizer->tokenize($serializedData) as $token) {
+            if (Token::STRING === $token->getType() && strstr($token->getValue(), $searchTerm)) {
+                $count += substr_count($token->getValue(), $searchTerm);
+            }
+        }
+
+        return $count;
+    }
+
+    public function replace(
+        string $serializedData,
+        string $searchTerm,
+        string $replaceTerm
+    ): string {
+        $tokenizer = new Tokenizer();
+
+        $tokenList = $tokenizer->tokenize($serializedData);
+        foreach ($tokenList as $index => $token) {
+            if (Token::STRING === $token->getType()) {
+                $tokenList[$index] = new Token(
+                    Token::STRING,
+                    str_replace($searchTerm, $replaceTerm, $token->getValue())
+                );
+            }
+        }
+
+        return join('', $tokenList);
+    }
+}
---
+++ tests/EditorTest.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright   (c) 2017-present brian ridley
+ * @author      brian ridley <ptlis@ptlis.net>
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\SerializedDataEditor\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ptlis\SerializedDataEditor\Editor;
+
+final class EditorTest extends TestCase
+{
+    public function testContains(): void
+    {
+        $editor = new Editor();
+
+        $this->assertTrue($editor->contains(
+            'O:3:"Foo":2:{s:3:"bar";s:6:"wibble";s:3:"baz";a:2:{i:0;s:6:"wibble";i:1;s:6:"wobble";}}',
+            'wibble'
+        ));
+    }
+
+    public function testDoesNotContain(): void
+    {
+        $editor = new Editor();
+
+        $this->assertFalse($editor->contains(
+            'O:3:"Foo":2:{s:3:"bar";s:6:"wibble";s:3:"baz";a:2:{i:0;s:6:"wibble";i:1;s:6:"wobble";}}',
+            'foobar'
+        ));
+    }
+
+    public function testContainsCount(): void
+    {
+        $editor = new Editor();
+
+        $this->assertEquals(
+            2,
+            $editor->containsCount(
+                'O:3:"Foo":2:{s:3:"bar";s:6:"wibble";s:3:"baz";a:2:{i:0;s:6:"wibble";i:1;s:6:"wobble";}}',
+                'wibble'
+            )
+        );
+    }
+
+    public function testContainsCountMultipleInString(): void
+    {
+        $editor = new Editor();
+
+        $this->assertEquals(
+            4,
+            $editor->containsCount(
+                'O:3:"Foo":3:{s:3:"bar";s:6:"wibble";s:3:"baz";a:2:{i:0;s:6:"wibble";i:1;s:6:"wobble";}s:3:"bat";a:1:{i:0;s:21:"wibble another wibble";}}',
+                'wibble'
+            )
+        );
+    }
+
+    public function testReplace(): void
+    {
+        $editor = new Editor();
+
+        $this->assertEquals(
+            'O:3:"Foo":3:{s:3:"bar";s:6:"foobar";s:3:"baz";a:2:{i:0;s:6:"foobar";i:1;s:6:"wobble";}s:3:"bat";a:1:{i:0;s:21:"foobar another foobar";}}',
+            $editor->replace(
+                'O:3:"Foo":3:{s:3:"bar";s:6:"wibble";s:3:"baz";a:2:{i:0;s:6:"wibble";i:1;s:6:"wobble";}s:3:"bat";a:1:{i:0;s:21:"wibble another wibble";}}',
+                'wibble',
+                'foobar'
+            )
+        );
+    }
+}
